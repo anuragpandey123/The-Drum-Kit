7d36a7fa4bf3807ebca84cda2460a622408db1f9
Anurag Pandey <53045130+anuragpandey123@users.noreply.github.com>
2021-03-23 16:33:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+
+ var number_drumcount= document.querySelectorAll(".drum").length;
+for( var i=0;i<number_drumcount;i++)
+{
+  //by button
+document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+
+
+  var buttonInnerHTML = this.innerHTML;
+  makeSound(buttonInnerHTML);
+  buttonAnimation(buttonInnerHTML);
+
+
+});
+}
+
+//by keyboard
+document.addEventListener("keypress", function(event){
+
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
+});
+
+function makeSound(key){
+  switch (key) {
+
+    case "w":
+      var audio= new Audio('crash.mp3');
+      audio.play();
+        break;
+    case "a":
+        var audio= new Audio('kick-bass.mp3');
+        audio.play();
+          break;
+    case "s":
+          var audio= new Audio('snare.mp3');
+          audio.play();
+            break;
+    case "d":
+            var audio= new Audio('tom-1.mp3');
+            audio.play();
+              break;
+    case "j":
+      var audio= new Audio('tom-2.mp3');
+        audio.play();
+        break;
+    case "k":
+        var audio= new Audio('tom-3.mp3');
+        audio.play();
+        break;
+    case "l":
+        var audio= new Audio('tom-4.mp3');
+        audio.play();
+        break;
+    default:
+
+  }
+
+}
+function buttonAnimation(currentkey){
+  var activeButton = document.querySelector("."+ currentkey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  },100);
+    }
+/*
+*/
